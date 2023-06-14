244febb74d327cdf65c3888782bcfd0e8fd0bbbf
JessicaOliveira <elisa0js@gmail.com>
2023-06-14 11:31:26 -0300

    manutenção das perguntas

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@ const perguntasERespostas = document.querySelectorAll(".item");
 perguntasERespostas.forEach(function (item) {
 	item.addEventListener("click", function () {
 		const itemAtivoAtual = document.querySelector(".ativo");
+		item.classList.add("ativo");
 		if (itemAtivoAtual) {
 			itemAtivoAtual.classList.remove("ativo");
 		}
 
-		item.classList.add("ativo");
+		
 	});
 });
